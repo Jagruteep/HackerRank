21834c525f80c5a823bae72fef601ac3cdf43986
Jagrutee Palande <jagpalande@gmail.com>
2018-02-21 15:42:09 +0530

    Create CoordDist.java

---
+++ CoordDist.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+public class CoordDist {
+
+    public static void main(String args[])
+    {
+       Scanner sc = new Scanner(System.in);
+        String coord = sc.nextLine();
+        String newstr = coord.replaceAll("[\\.$|,|;|(|)']","");
+        String[] parts = newstr.split(" ");
+        double x1 = Double.parseDouble(parts[0]);
+        double y1 = Double.parseDouble(parts[1]);
+        double x2 = Double.parseDouble(parts[2]);
+        double y2 = Double.parseDouble(parts[3]);
+        
+        double dist = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+        int val = (int)(dist);
+        System.out.println(val);
+    }
+
+}
