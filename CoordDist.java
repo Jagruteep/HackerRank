54d4958a630831a4e52fa0e2c72567d67ed8d3ff
Jagrutee Palande <jagpalande@gmail.com>
2018-02-22 23:19:51 +0530

    Update CoordDist.java

--- CoordDist.java
+++ CoordDist.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class CoordDist {
 
-    public static void main(String args[])
+    public static void main(String args[]) throws IOException
     {
        Scanner sc = new Scanner(System.in);
         String coord = sc.nextLine();
